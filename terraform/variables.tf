c72c72d56ffae19b98cb163d0f3e038eaeeaddb1
Derick Etoe Etuge <dericketoe.dev@gmail.com>
2026-08-03 17:51:06 +0100

    Initialize Terraform project

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "description"
+}
+
+variable "project_name" {
+  type        = string
+  default     = "aws-enterprise-api"
+  description = "description"
+}
+
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "description"
+}
+
