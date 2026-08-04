133da589023f60e102c8c2a6b16cc3bf5a1ced06
Derick Etoe Etuge <dericketoe.dev@gmail.com>
2026-08-04 16:49:49 +0100

    change terraform format

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,19 +1,19 @@
+
+
 variable "aws_region" {
   type        = string
   default     = "us-east-1"
-  description = "description"
+  description = "aws_region - description"
 }
 
 variable "project_name" {
   type        = string
   default     = "aws-enterprise-api"
-  description = "description"
+  description = "project_name - description"
 }
-
 
 variable "environment" {
   type        = string
   default     = "dev"
-  description = "description"
+  description = "environment - description"
 }
-
